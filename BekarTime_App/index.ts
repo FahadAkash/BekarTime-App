56cabc74b6f6f8a57a0e4a9083281e08d3272247
Fahad Akash <32955702+FahadAkash@users.noreply.github.com>
2025-07-10 13:20:19 +0600

    remove  mainapp

--- BekarTime_App/index.ts
+++ BekarTime_App/index.ts
@@ -1,10 +1,8 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import Test from './Test';
-import MapScreen from './app/MapScreen';
 import MainApp from './MainApp';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(MainApp);
+registerRootComponent(App);
